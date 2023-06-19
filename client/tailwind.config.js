9d3b19472abb38266ec83fc2ef6029c7cf201804
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-06-19 12:39:05 -0400

    Add tailwind custom color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'pink-light': '#E6A7AF',
+      'pink-medium': '#E96D77',
+      'pink-dark': '#8F4465',
+      'purple-light': '#E3E0E7',
+      'purple-medium': '#998BA8',
+      'purple-dark': '#351A53',
+      'dark': '#1E142A',
+      'light': '#F5F3F6',
+    },
     extend: {},
   },
   plugins: [],
