085f41988d72b4ad3ad19538023c9c9fe888541f
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-06-22 12:47:17 -0400

    Update NextDay card component and sample data

--- client/src/components/NextDay.js
+++ client/src/components/NextDay.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 
-const NextDay = () => {
+const NextDay = (props) => {
+  let [lowTemp, setLowTemp] = useState(props.lowTemp);
+  let [highTemp, setHighTemp] = useState(props.highTemp);
+  let [day, setDay] = useState(props.day)
+  let [weatherForecast, setWeatherForecast] = useState(props.weatherForecast)
+  let [backgroundUrl, setBackgroundUrl] = useState(props.backgroundUrl)
+  let [isExpanded, setIsExpanded] = useState(false)
+
   return (
     <div>
-        <div>
-            <h5>Next Day</h5>
+        <h5>Next Day</h5>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="" alt="Weather status" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">{day}</h2>
+            <div>
+              <p>Low: {lowTemp}</p>
+              <p>High: {highTemp}</p>
+              <p>weatherStatus: {weatherForecast}</p>
+              <p className="overflow-hidden">backgroundUrl: {backgroundUrl}</p>
+            </div>
+            <div className="card-actions justify-end">
+              <button className="btn btn-sm">Expand for more</button>
+            </div>
+          </div>
         </div>
     </div>
   )
--- client/src/components/NextDays.js
+++ client/src/components/NextDays.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import NextDay from './NextDay'
+import forecastData from '../data/forecast_data.json'
 
 const NextDays = () => {
   return (
     <div>
-      <NextDay />
-      <NextDay />
-      <NextDay />
-      <NextDay />
-      <NextDay />
-      <NextDay />
-      <NextDay />
+      {/* <NextDay lowTemp={56} highTemp={78} day="Thursday" weatherForecast="Sunny" backgroundUrl="www.google.com" /> */}
+      {
+        forecastData.map(( {lowTemp, highTemp, day, weatherForecast, backgroundUrl }, index) =>
+         <NextDay key={index}
+          lowTemp={lowTemp} highTemp={highTemp} day={day} weatherForecast={weatherForecast} backgroundUrl={backgroundUrl} />)
+      }
     </div>
   )
 }
