1573442cb96d4f0d978a62b8dea7137d750f242c
Marko Vasic <122864019+Vastradamus@users.noreply.github.com>
2023-06-25 17:10:02 +0200

    Changed some line around MongoDB, so the server can run propertly

--- server/server.js
+++ server/server.js
@@ -3,17 +3,16 @@ const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const connectMongo = require('connect-mongo'); // changed from Leons wersion
 const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const todoRoutes = require('./routes/todos')
 
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
-require('./config/passport')(passport)
+
+// require('./config/passport')(passport) i commented this line since the passport is currently empty
 
 connectDB()
 
@@ -25,21 +24,21 @@ app.use(logger('dev'))
 
 // Sessions
 app.use(
-    session({
+  session({
       secret: 'keyboard cat',
       resave: false,
       saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
-  
+      store: connectMongo.create({ mongoUrl: process.env.DB_STRING })
+  })
+);
+
+
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
-
 app.use(flash())
 
 app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+  console.log(`Server is running on a port ${process.env.PORT}, you better catch it!`)
+})  
