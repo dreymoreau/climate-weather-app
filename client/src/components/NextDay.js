880d877fe26392c5cb897fbbcb3b9809dd115e95
dreymotoh <striclyspoken@gmail.com>
2023-07-03 11:10:49 -0400

    added photos for different states

--- client/src/components/NextDay.js
+++ client/src/components/NextDay.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { useState } from 'react'
 import {
-  PiCloudDuotone,
   PiCloudFogDuotone,
   PiCloudLightningDuotone,
-  PiCloudMoonDuotone,
   PiCloudRainDuotone,
   PiCloudSnowDuotone,
   PiCloudSunDuotone,
